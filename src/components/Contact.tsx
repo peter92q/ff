6447a94663205960c9fa610d3a6ad7c5d1ef5f26
peter92q <114609817+peter92q@users.noreply.github.com>
2023-11-05 00:29:32 +0200

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -74,28 +74,13 @@ const Contact = ({setSelectedPage}:{setSelectedPage: React.Dispatch<SetStateActi
         variants={slideIn("left", "tween", 0.2, 1)}
         className={`xl:mt-12 flex xl:flex-row flex-col-reverse mx-auto p-[1px] violet-gradient rounded-xl gap-10 overflow-hidden`}
       >
-        <div
-          
-          className='flex-1 bg-black-100 p-8 rounded-xl flex flex-col md:flex-row bg-[#1c1733]'
+        <div 
+          className='flex-1 p-8 rounded-xl flex flex-col md:flex-row bg-[#1c1733]'
         >
         <div>
-        <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
-        <div className="flex flex-row mb-4">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
-        </svg>
-
-        <h4 className="font-bold ml-1">Email: exeption111@gmail.com</h4>
-        </div>
-        <div className="flex flex-row">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
-            </svg>
-
-            <h4 className="font-bold ml-1">Phone: +353 89 953 0926</h4>
-        
-       </div>
+   
+        <h3 className={styles.sectionHeadText}>Contact</h3>
+       
        </div>
        <form
           onSubmit={handleSubmit}
@@ -135,10 +120,9 @@ const Contact = ({setSelectedPage}:{setSelectedPage: React.Dispatch<SetStateActi
               className='bg-[#0e0c1e] py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
-
           <button
             type='submit'
-            className='bg-[#0e0c1e] py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            className='py-3 bg-gray-800 hover:bg-gray-700 px-8 rounded-xl outline-none w-fit text-white font-bold '
           >
             {loading ? "Sending..." : "Send"}
           </button>
