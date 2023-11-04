964c199c3751f163b9eefd37b865308fddc1aa43
peter92q <114609817+peter92q@users.noreply.github.com>
2023-11-05 00:36:48 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Certificates, Feedbacks, Hero, Navbar, Projects } from "./components";
-import { useLayoutEffect, useMemo, useState } from "react";
-import battle2Image from '../src/assets/battle2.png';
+import { useState } from "react";
 
 const App = () => {
   const [selectedPage, setSelectedPage] = useState<string | null>(null)
-  const [isImageLoaded, setImageLoaded] = useState(false);
-  
-  const loadImage = useMemo(() => {
-    const image = new Image();
-    image.src = battle2Image;
-
-    image.onload = () => {
-      setImageLoaded(true);
-    };
-    return () => {
-      image.onload = null;
-    };
-  }, []);
-  
-  useLayoutEffect(() => {
-    return loadImage;
-  }, [loadImage]);
 
   return (
     <BrowserRouter>
       <div className='relative z-0 dd overflow-hidden'>
-        <div 
-          style={{ backgroundImage: `url(${battle2Image})` }}
-          className={`bg-no-repeat bg-image ${isImageLoaded ? 'loaded' : ''}`}>
+        <div
+          className="bg-battle bg-cover"
+        >
           <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
           <Hero setSelectedPage={setSelectedPage}/>  
         </div> 
